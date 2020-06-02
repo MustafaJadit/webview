2cde7f8814b9c228218750b267f02bec3d6becb3
mustafaatush <mustafaatush@gmail.com>
2020-06-01 19:19:06 -0700

    added webview

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.mwebview);
+        webView.loadUrl("https://www.google.com");
     }
 }
