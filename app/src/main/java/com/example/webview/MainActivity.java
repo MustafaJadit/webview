b97b29589d6f3f755d84008efde85c094953398e
mustafaatush <mustafaatush@gmail.com>
2020-06-02 17:23:37 -0700

    added two ways of adding delay tasks (unrelated content)

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.webkit.WebView;
 
@@ -15,5 +17,51 @@ public class MainActivity extends AppCompatActivity {
         WebView webView = findViewById(R.id.mwebview);
         webView.loadUrl("https://shinjuku-musasino.net/");
         webView.getSettings().setJavaScriptEnabled(true);
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+
+        }, 200);
+
+
+        // webview Unrealted code, haha
+        twoWaysOfTaskDelaying();
+    }
+
+    private void twoWaysOfTaskDelaying() {
+        AsyncTask stringStringStringAsyncTask = new AsyncTask<String, String, String>() {
+
+            @Override
+            protected String doInBackground(String... strings) {
+                return null;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+            }
+
+            @Override
+            protected void onProgressUpdate(String... values) {
+                super.onProgressUpdate(values);
+            }
+        };
+
+        stringStringStringAsyncTask.execute("d");
+
+
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread.start();
     }
 }
